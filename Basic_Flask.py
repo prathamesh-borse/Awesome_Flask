53f49e45c9482682d1b76cbc2c7e5448f92c9056
Prathamesh Borse <PrathameshBorse.official@gmail.com>
2021-01-17 14:04:36 +0530

    Added Files

---
+++ Basic_Flask.py
@@ -0,0 +1,39 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+	if request.method == "POST":
+		session.permanent = True
+		user = request.form["nm"]
+		session["user"] = user
+		return redirect(url_for("user"))
+	else:
+		if "user" in session:
+			return redirect(url_for("user"))
+
+		return render_template("login.html")
+
+@app.route("/user")
+def user():
+	if "user" in session:
+		user = session["user"]
+		return f"<h1>{user}</h1>"
+	else:
+		return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+	session.pop("user", None)
+	return redirect(url_for("login"))
+
+if __name__ == "__main__":
+	app.run(debug=True)
